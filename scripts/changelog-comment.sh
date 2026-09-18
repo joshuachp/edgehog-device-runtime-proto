e26d221b79222263d58ede6afcb7fc3e4586f09e
Joshua Chapman <joshua.chapman@secomind.com>
2026-09-18 14:46:00 +0200

    feat(config): add configuration service
    This will permit to dynamically configure the Edgehog Device Runtime.
    
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- scripts/changelog-comment.sh
+++ scripts/changelog-comment.sh
@@ -11,7 +11,6 @@ trap 'echo "Exit status $? at line $LINENO from: $BASH_COMMAND"' ERR
 
 FROM=$1
 TO=$2
-PR=$3
 
 if [[ -n ${GITHUB_REPO:-} ]]; then
     changelog=$(git cliff -s all --github-repo="$GITHUB_REPO" -- "$FROM..$TO")
@@ -31,4 +30,4 @@ else
     body="The commits don't specify any user facing change to add to the CHANGELOG.md."
 fi
 
-gh pr comment "$PR" --edit-last --create-if-none --body="$body"
+echo "$body" >>"$GITHUB_STEP_SUMMARY"
